a7e9f159738fa6162861cdda477418f27f0c4a2d
bhb-del <944029862@qq.com>
2022-03-11 12:15:21 +0800

    web3 problem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom"
 import { Web3ReactProvider, useWeb3React } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from "@web3-react/injected-connector";
+import Web3 from "web3";
+import abi from "../Wallet/js/abi.js"
+// import { library } from "webpack";
 
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider)
@@ -25,10 +28,20 @@ const App = () => {
   const injected = new InjectedConnector({
     supportedChainIds: [1, 3, 4, 5, 42]
   })
+
   const { chainId, account, activate } = useWeb3React();
   const connect = () => {
     activate(injected);
   }
+
+  const contractAddress = "0xF91995dDb1FA78C0727C994f28e5befD70528C34";
+  async function Mint(account, val) {
+    let web3 = new Web3(window.web3.currentProvider);
+    let contract = new web3.eth.Contract(abi, contractAddress)
+    contract.methods.mint(account, val);
+  }
+
+
   return (
     <div className={styles.main}>
       <div className={styles.text}>
@@ -37,6 +50,7 @@ const App = () => {
       <p>ChainId: {chainId}</p>
       <p>Account: {account}</p>
       <button type="button" onClick={connect}>Connect</button>
+      <button type="button" onClick={() => { Mint(account, "1000000000000000000") }}>Mint</button>
     </div >
   )
 }
